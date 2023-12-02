66321de6935d7211c8130ed986f40bee6752e964
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-12-01 20:12:38 -0800

    day 1 solution

--- day-1/index-2.js
+++ day-1/index-2.js
@@ -1,11 +1,52 @@
 const fs = require('fs');
 
 const data = fs.readFileSync('/Users/ramitaarora/Documents/GitHub/advent-of-code-2023/day-1/input.txt', 'utf-8');
-const strings = data.split('\n')
+const strings = data.split('\n');
+const spelledNums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
 let numArray = [];
 let total = 0;
 let newArray = [];
+
+function checkNumber(num) {
+    switch(num) {
+        case 'one':
+        return 1;
+        break;
+
+        case 'two':
+        return 2;
+        break;
+
+        case 'three':
+        return 3;
+        break;
+
+        case 'four':
+        return 4;
+        break;
+
+        case 'five':
+        return 5;
+        break;
+
+        case 'six':
+        return 6;
+        break;
+
+        case 'seven':
+        return 7;
+        break;
+
+        case 'eight':
+        return 8;
+        break;
+
+        case 'nine':
+        return 9;
+        break;
+    }
+}
 
 
 function calculateTotal() {
@@ -36,6 +77,9 @@ function makeArray() {
 function main() {
     for (let i = 0; i < strings.length; i++) {
         let oneLine = strings[i]
+        if (oneLine.includes(spelledNums)) {
+            
+        }
         // console.log(oneLine);
         let numbers = '';
         for (let j = 0; j < oneLine.length; j++) {
