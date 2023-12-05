6aea981d0fff6531296c811ff28f99ffeb3309ec
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-12-04 20:31:39 -0800

    day 3

--- day-3/index.js
+++ day-3/index.js
@@ -2,14 +2,33 @@ const fs = require('fs');
 
 const data = fs.readFileSync('/Users/ramitaarora/Documents/GitHub/advent-of-code-2023/day-3/input.txt', 'utf-8');
 const oneLine = data.split('\n')
-console.log(oneLine)
+
+let symbolIndices = [];
+let newLine = [];
+
+for (let i = 0; i < oneLine.length; i++) {
+    newLine.push(oneLine[i].split('.'))
+} console.log(newLine)
+
+for (let j = 0; j < newLine.length; j++) {
+    if (/[!@#$%^&*/+=-]/.test(newLine[j])) {
+        symbolIndices.push(j)
+        
+    }
+    if (/^[0-9]*$/.test(newLine[j])) {
+        
+    }
+} 
+
+
+
 
 // if (/[!@#$%^&*/+=-]/.test(oneLine[i][j]) === );
 
-for (let i = 0; i < oneLine.length; i++) {
-    for (let j = 0; j < oneLine[i].length; j++) {
-        if (/^[0-9]*$/.test(oneLine[i][j])) {
+// for (let i = 0; i < oneLine.length; i++) {
+//     for (let j = 0; j < oneLine[i].length; j++) {
+//         if (/^[0-9]*$/.test(oneLine[i][j])) {
             
-        }
-    }
-}
+//         }
+//     }
+// }
